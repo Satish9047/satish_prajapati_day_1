3e2eaddbb559f8cbca9b8dc9d393b9ed36083c78
Satish9047 <satishprajapati930@gmail.com>
2023-12-27 10:34:04 +0545

    error occured fixed

---
+++ sql/seeds/insert-shippers.sql
@@ -0,0 +1,3 @@
+INSERT INTO shippers (shipper_id, shipper_name, phone) VALUES ('1', 'Speedy Express', '(503) 555-9831');
+INSERT INTO shippers (shipper_id, shipper_name, phone) VALUES ('2', 'United Package', '(503) 555-3199');
+INSERT INTO shippers (shipper_id, shipper_name, phone) VALUES ('3', 'Federal Shipping', '(503) 555-9931');
